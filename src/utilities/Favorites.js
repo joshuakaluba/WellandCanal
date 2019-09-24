0486e523de84646e5fb6ea9260a5ab72fbed420c
Joshua Kaluba <joshkaluba@gmail.com>
2019-09-23 20:11:46 -0400

    Updated storage workflow. Switched from parsing a '|' demiminated string to storing JSON

--- src/utilities/Favorites.js
+++ src/utilities/Favorites.js
@@ -1,68 +1,47 @@
 import { AsyncStorage, Vibration } from "react-native";
 
-const FAV_STORAGE_KEY = "WellandCanalFavoritesStorageKey";
+const FAV_STORAGE_KEY = "WELLAND_CANAL_FAV_KEY";
 
-export default (Favorites = {
-  async saveFavorites(favToSav) {
-    let favorites = await this.getSplitFavorites();
-
-    if (!favorites.includes(`${favToSav}`)) {
-      let favString = await this.getFavorites();
-
-      var newFav = `${favString}|${favToSav}|`;
-
-      newFav = newFav.replace("||", "|");
-
-      await AsyncStorage.setItem(FAV_STORAGE_KEY, newFav);
-    }
+export default Favorites = {
+  async saveFavorites(favoriteId) {
+    let favorites = await this.getFavorites();
+    favorites.push({ id: favoriteId });
+    await AsyncStorage.setItem(FAV_STORAGE_KEY, JSON.stringify(favorites));
   },
 
-  async removeFav(favoriteToRemove) {
-    let favorites = await this.getSplitFavorites();
+  async removeFavorite(favoriteId) {
+    const favorites = await this.getFavorites();
+    var filtered = favorites.filter(favorite => {
+      return favorite.id != favoriteId;
+    });
 
-    while ((i = favorites.indexOf(`${favoriteToRemove}`)) != -1) {
-      favorites.splice(i, 1);
-    }
-
-    await AsyncStorage.removeItem(FAV_STORAGE_KEY);
-
-    for (i = 0; i < favorites.length; i++) {
-      await this.saveFavorites(favorites[i]);
-    }
+    await AsyncStorage.setItem(FAV_STORAGE_KEY, JSON.stringify(filtered));
   },
 
   async getFavorites() {
-    let value = (await AsyncStorage.getItem(FAV_STORAGE_KEY)) || "|";
-    return value;
-  },
-
-  async getSplitFavorites() {
-    let favorite = await this.getFavorites();
-    favorite = favorite.substring(1, favorite.length - 1);
-    let value = favorite.split("|");
-
-    if (value.length < 1) {
-      return {};
+    const favorites = await AsyncStorage.getItem(FAV_STORAGE_KEY);
+    if (favorites && favorites.length > 0) {
+      return JSON.parse(favorites);
     }
-
-    return value;
+    return [];
   },
 
   async handleFavoritesClick(item) {
     Vibration.vibrate(200);
-    const favorites = await this.getSplitFavorites();
-    let found = false;
+    const favorites = await this.getFavorites();
+    console.log(item);
 
-    for (i = 0; i < favorites.length; i++) {
-      if (favorites[i] == item.bridge_id) {
-        found = true;
-        break;
-      }
-    }
-    if (found == false) {
-      await this.saveFavorites(item.bridge_id);
+    var result = favorites.find(obj => {
+      return obj.id === item.bridge_id;
+    });
+
+    if (!result) {
+      console.log(`Saving: ${item.bridge_id}`);
+      await this.saveFavorites(item.bridge_id);      
     } else {
-      await this.removeFav(item.bridge_id);
+      console.log(`Removing: ${item.bridge_id}`);
+      await this.removeFavorite(item.bridge_id);
     }
   }
-});
+
+};
