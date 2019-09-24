b43a6e416636522105664e80783af7fff8b8d252
Joshua Kaluba <joshkaluba@gmail.com>
2019-09-23 20:10:38 -0400

    Code cleanup

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
-import { AppLoading, Asset, Font, Icon } from 'expo';
-import AppNavigator from './navigation/AppNavigator';
+import React from "react";
+import { Platform, StatusBar, StyleSheet, View } from "react-native";
+import { AppLoading } from "expo";
+import * as Font from "expo-font";
+import * as Icon from "@expo/vector-icons";
+import AppNavigator from "./src/navigation/AppNavigator";
 
 export default class App extends React.Component {
   state = {
-    isLoadingComplete: false,
+    isLoadingComplete: false
   };
 
   render() {
@@ -20,7 +22,7 @@ export default class App extends React.Component {
     } else {
       return (
         <View style={styles.container}>
-          {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+          {Platform.OS === "ios" && <StatusBar barStyle="dark-content" />}
           <AppNavigator />
         </View>
       );
@@ -29,13 +31,9 @@ export default class App extends React.Component {
 
   _loadResourcesAsync = async () => {
     return Promise.all([
-      Asset.loadAsync([
-        require('./assets/images/robot-dev.png'),
-        require('./assets/images/robot-prod.png'),
-      ]),
       Font.loadAsync({
         ...Icon.Ionicons.font
-      }),
+      })
     ]);
   };
 
@@ -51,6 +49,6 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-  },
+    backgroundColor: "#fff"
+  }
 });
