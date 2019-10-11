b056c56f1746d5948d19094b90068c3627bccad5
Joshua Kaluba <joshkaluba@gmail.com>
2019-10-10 20:07:04 -0400

    Create index.js

---
+++ src/screens/index.js
@@ -0,0 +1,13 @@
+import UpcomingClosuresScreen from "./UpcomingClosuresScreen";
+import BridgeStatusScreen from "./BridgeStatusScreen";
+import BulletinScreen from "./BulletinScreen";
+import FavoritesScreen from "./FavoritesScreen";
+import MapScreen from "./MapScreen";
+
+export {
+  UpcomingClosuresScreen,
+  BridgeStatusScreen,
+  BulletinScreen,
+  FavoritesScreen,
+  MapScreen
+};
