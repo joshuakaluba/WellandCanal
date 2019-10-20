bd2c1f486371821bc7f00c83e33562db6375c085
Joshua Kaluba <joshkaluba@gmail.com>
2019-10-20 09:00:10 -0400

    V1 i18n

---
+++ src/constants/StringDictionary.js
@@ -0,0 +1,12 @@
+export default (StringDictionary = {
+  bridgeStatus: "Bridge Status",
+  myFavorites: "My Favorites",
+  nextClosure: "Next Closure: ",
+  mapStatus: "Map Status",
+  noClosuresFound: "No closures found...",
+  noUpcomingClosures: "Currently no planned upcoming closures",
+  noFavorites:
+    "Currently no favorites have been set. Please add favorites from the 'Bridge Status' page",
+  upComingClosures: "Upcoming Closures",
+  noDataCurrentlyAvailable: "No data currently available. Please refresh"
+});
