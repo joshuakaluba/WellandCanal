21f512c19d483a6f1eaeb87249c0a5f9040d840f
Joshua Kaluba <joshkaluba@gmail.com>
2019-10-20 08:59:51 -0400

    Created master object for colors

---
+++ src/constants/Colors.js
@@ -0,0 +1,39 @@
+const tintColor = "#000";
+const iconDefault = "#fff";
+const mint = "#72AD97";
+const tabBackgroundColor = mint;
+const white = "#fff";
+const black = "#000";
+const darkGrey = "#586069";
+const lightGrey = "#bdc3c7";
+const tabInactiveTintColor = darkGrey;
+const orange = "#f39c12";
+
+
+export default {
+  tintColor,
+  iconDefault,
+  white,
+  black,
+  orange,
+  headerTintColor: white,
+  darkGrey,
+  tabIconDefault: tabInactiveTintColor,
+  tabIconSelected: iconDefault,
+  tabBackgroundColor,
+  tabActiveTintColor: white,
+  tabInactiveTintColor,
+  activityIndicator: "tomato",
+  headerBackgroundColor: tabBackgroundColor,
+  tabBar: "red",
+  lightGrey,
+  errorBackground: "red",
+  listBackgroundColor: "#fff",
+  errorText: "#fff",
+  warningBackground: "#EAEB5E",
+  warningText: "#666804",
+  noticeBackground: tintColor,
+  noticeText: "#fff",
+  success: mint,
+  danger: "#D16C5C"
+};
